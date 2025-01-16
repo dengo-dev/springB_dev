16f20339cbdb5e0b410f5f9c9c65eabcdea648bb
dengo <dengo23.dev@gmail.com>
2025-01-16 09:50:55 +0900

    초기설정

---
+++ src/main/java/me/dengo/springb/SpringBDeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.dengo.springb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBDeveloperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBDeveloperApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/dengo/springb/SpringBDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dengo.springb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBDeveloperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
